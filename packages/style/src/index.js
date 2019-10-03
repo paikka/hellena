6780f09e1dee083c3ee64db5c2dccbc07b28863e
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-10-03 16:36:40 -0400

    Adicionado variáveis para tipo, tamanho e espessura de fontes na estrutura do aquivos sass

---
+++ packages/style/src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Copyright Paikka. 2019, 2019
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export * from './utilities/variables';
---
+++ packages/style/src/utilities/variables.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright Paikka. 2019, 2019
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// Typography
+//
+//
+
+// Typography / Font Family
+export const fontFamilySansSerif = '\'IBM Plex Sans\', sans-serif';

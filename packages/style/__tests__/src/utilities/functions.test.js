48ed031c0185853ffef06e7ef57dfd474bd6a527
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-10-04 15:32:11 -0400

    Atualizado testes para variáveis de tipografia

--- packages/style/__tests__/src/utilities/functions.test.js
+++ packages/style/__tests__/src/utilities/functions.test.js
@@ -11,10 +11,10 @@
 
 import {rgba} from "../../../src/utilities/functions";
 
-test('convert color #000000 to rgba(0, 0, 0, 0.50)', () => {
+test('Convert the HEX color #000000 to RGBA rgba(0, 0, 0, 0.50)', () => {
     expect(rgba('#000000', '0.50')).toEqual('rgba(0, 0, 0, 0.50)');
 });
 
-test('convert color #FFFFFF to rgba(255, 255, 255, 0.90)', () => {
+test('Convert the HEX color #FFFFFF to RGBA rgba rgba(255, 255, 255, 0.90)', () => {
     expect(rgba('#FFFFFF', '0.90')).toEqual('rgba(255, 255, 255, 0.90)');
 });
--- packages/style/__tests__/src/utilities/variables.test.js
+++ packages/style/__tests__/src/utilities/variables.test.js
@@ -3,14 +3,41 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- *
- * @jest-environment node
  */
+
 
 'use strict';
 
 import {variables} from '../../../src';
+import {fontSizes} from "../../../src";
+import {fontWeights} from "../../../src";
 
 test('variables', () => {
     expect(variables).toMatchSnapshot();
+});
+
+test('Font size object assignment', () => {
+    const expected = {
+        1: '.5rem',
+        2: '.875rem',
+        3: '1rem',
+        4: '1.125rem',
+        5: '1.25rem',
+        6: '1.5rem',
+        7: '1.75rem',
+        8: '2rem',
+        9: '2.25rem',
+        10: '2.625rem',
+        11: '3rem',
+        12: '3.375rem',
+        13: '3.75rem',
+        14: '4.25rem',
+        15: '4.75rem'
+    };
+    expect(fontSizes).toEqual(expected);
+});
+
+test('Font weight object assignment', () => {
+    const expected = {1: 300, 2: 400, 3: 600, 4: 700, 5: 800};
+    expect(fontWeights).toEqual(expected);
 });

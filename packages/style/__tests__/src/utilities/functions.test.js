bbe3761336a2617e7a3146375be3254e4b92e930
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-10-04 14:52:10 -0400

    Atualizado arquivo de funções

--- packages/style/__tests__/src/utilities/functions.test.js
+++ packages/style/__tests__/src/utilities/functions.test.js
@@ -1,3 +1,14 @@
+/**
+ * Copyright Paikka. 2019, 2019
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @jest-environment node
+ */
+
+'use strict';
+
 import {rgba} from "../../../src/utilities/functions";
 
 test('convert color #000000 to rgba(0, 0, 0, 0.50)', () => {

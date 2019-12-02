bbfed3e2f8f6f4ef7d899df401570cb660c91b54
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-12-02 17:01:26 -0400

    Adicionado variáveis de cores primárias ao arquivo de script

---
+++ packages/colors/src/colors.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright Paikka. 2019, 2019
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export const black = '#000000';
+
+export const white = '#ffffff';
+
+export const primary100 = '#CCDDED';
+export const primary200 = '#99BCDB';
+export const primary300 = '#3993DA';
+export const primary400 = '#166BC3';
+export const primary500 = '#166BC3';
+export const primary600 = '#166BC3';
+export const primary = {
+  100: primary100,
+  200: primary200,
+  300: primary300,
+  400: primary400,
+  500: primary500,
+  600: primary600
+};
+

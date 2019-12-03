18022a752198f963c4c1de10343cb2fdbb523193
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-12-03 12:26:50 -0400

    Adicionado variáveis para tipografia

---
+++ packages/typography/src/typography.js
@@ -0,0 +1,88 @@
+/**
+ * Copyright Paikka. 2019, 2019
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export const baseFontSize = '16px';
+
+export const fontSize1 = '.5rem';
+export const fontSize2 = '.875rem';
+export const fontSize3 = '1rem';
+export const fontSize4 = '1.125rem';
+export const fontSize5 = '1.25rem';
+export const fontSize6 = '1.5rem';
+export const fontSize7 = '1.75rem';
+export const fontSize8 = '2rem';
+export const fontSize9 = '2.25rem';
+export const fontSize10 = '2.625rem';
+export const fontSize11 = '3rem';
+export const fontSize12 = '3.375rem';
+export const fontSize13 = '3.75rem';
+export const fontSize14 = '4.25rem';
+export const fontSize15 = '4.75rem';
+export const fontSize = {
+  1: fontSize1,
+  2: fontSize2,
+  3: fontSize3,
+  4: fontSize4,
+  5: fontSize5,
+  6: fontSize6,
+  7: fontSize7,
+  8: fontSize8,
+  9: fontSize9,
+  10: fontSize10,
+  11: fontSize11,
+  12: fontSize12,
+  13: fontSize13,
+  14: fontSize14,
+  15: fontSize15
+};
+
+export const fontWeight1 = 100;
+export const fontWeight2 = 200;
+export const fontWeight3 = 300;
+export const fontWeight4 = 400;
+export const fontWeight5 = 500;
+export const fontWeight6 = 600;
+export const fontWeight7 = 700;
+export const fontWeight8 = 800;
+export const fontWeight9 = 900;
+export const fontWeight = {
+  1: fontSize1,
+  2: fontSize2,
+  3: fontSize3,
+  4: fontSize4,
+  5: fontSize5,
+  6: fontSize6,
+  7: fontSize7,
+  8: fontSize8,
+  9: fontSize9
+};
+
+export const text1 = fontSize1;
+export const text2 = fontSize2;
+export const text3 = fontSize3;
+export const text4 = fontSize4;
+export const text = {
+  1: fontSize1,
+  2: fontSize2,
+  3: fontSize3,
+  4: fontSize4
+};
+
+export const heading1 = fontSize5;
+export const heading2 = fontSize6;
+export const heading3 = fontSize7;
+export const heading4 = fontSize8;
+export const heading5 = fontSize9;
+export const heading6 = fontSize10;
+export const heading = {
+  1: fontSize5,
+  2: fontSize6,
+  3: fontSize7,
+  4: fontSize8,
+  5: fontSize9,
+  6: fontSize10
+};
